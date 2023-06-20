9b59f55b8a9e87785290e4985dcea7463f8207f8
Jon Webb <webbja123@gmail.com>
2023-06-20 08:08:11 -0600

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,48 @@
+************
+todo_five
+************
+
+Describe project here
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
+
+.. image:: https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336
+    :target: https://pycqa.github.io/isort/
+
+.. image:: https://readthedocs.org/projects/flake8/badge/?version=latest
+    :target: https://flake8.pycqa.org/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: http://www.mypy-lang.org/static/mypy_badge.svg
+   :target: http://mypy-lang.org/
+
+
+.. image:: https://github.com/Jon-Webb-79/todo_five/workflows/Tests/badge.svg?cache=none
+    :target: https://github.com/Jon-Webb-79/todo_five/actions
+
+Contributing
+############
+Pull requests are welcome.  For major changes, please open an issue first to discuss
+what you would like to change.  Please make sure to include and update tests
+as well as relevant doc-string and sphinx updates.
+
+License
+#######
+The License is included in the **project_name** package
+
+Requirements
+############
+Python 3.8 or greater
+List code package requirements here
+
+Installation
+############
+In order to download this repository from github, follow these instructions
+
+1. Ensure you have .git installed on your computer
+2. At your desired location create a directory titled ``todo_five``
+3. Open a terminal (Bash, zsh, Linux, or DOS) and cd to the ``todo_five`` directory and type
+   ``close https://github.com/Jon-Webb-79/todo_five.git todo_five``
+4. Install with pip3
+   ``pip3 install -r requirements``
---
+++ tests/widgets_test.py
@@ -0,0 +1,44 @@
+# Import necessary packages here
+
+# - If a package and a module within the package is to be imported
+#   uncomment the following lines where dir is the directory containing
+#   the source files.  These lines should go above the module imports
+# import sys
+# import os
+# sys.path.insert(1, os.path.abspath(dir))
+# ==========================================================================================
+# ==========================================================================================
+# File:    test.py
+# Date:    June 20, 2023
+# Author:  Jonathan A. Webb
+# Purpose: Describe the types of testing to occur in this file.
+# Instruction: This code can be run in hte following ways
+#              - pytest # runs all functions beginnning with the word test in the
+#                         directory
+#              - pytest file_name.py # Runs all functions in file_name beginning
+#                                      with the word test
+#              - pytest file_name.py::test_func_name # Runs only the function
+#                                                      titled test_func_name in
+#                                                      the file_name.py file
+#              - pytest -s # Runs tests and displays when a specific file
+#                            has completed testing, and what functions failed.
+#                            Also displays print statments
+#              - pytest -v # Displays test results on a function by function
+#              - pytest -p no:warnings # Runs tests and does not display warning
+#                          messages
+#              - pytest -s -v -p no:warnings # Displays relevant information and
+#                                supports debugging
+#              - pytest -s -p no:warnings # Run for record
+# ==========================================================================================
+# ==========================================================================================
+# Insert Code here
+
+
+def test_give_name_here():
+    # Add test here
+    pass
+
+
+# ==========================================================================================
+# ==========================================================================================
+# eof
